f2cebde77a96449170142352e92a62f0f8af6068
Declan Rocco <declanrocco@protonmail.com>
2024-11-21 20:31:30 +1000

    Require semi colons

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,7 @@
 import { configApp } from '@adonisjs/eslint-config'
-export default configApp()
+export default configApp({
+    rules: {
+        'semi': ['error', 'always', { 'omitLastInOneLineBlock': true, 'omitLastInOneLineClassBody': true }],
+        'prettier/prettier': ['error', { 'semi': true }],
+    }
+})
