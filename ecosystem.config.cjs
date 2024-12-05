c49cf48697112788e911dfb5ebffeae1f454f96f
Declan Rocco <declanrocco@protonmail.com>
2024-12-05 22:00:11 +1000

    Change build config location

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'octorand-api',
-            script: './bin/server.js',
+            script: './build/bin/server.js',
             instances: 'max',
             exec_mode: 'cluster',
             autorestart: true,
